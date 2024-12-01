676624b0e9e38bae9a55ef808a3a3498043040b7
Nattapat Srirung <157050379+JeabtiE@users.noreply.github.com>
2024-12-01 17:05:13 +0700

    Add files via upload

---
+++ scripts2.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Run when the page is fully loaded
+window.addEventListener("load", () => {
+    // Hide the loading screen
+    const loadingScreen = document.getElementById("loading-screen");
+    loadingScreen.style.display = "none";
+  
+    // Show the main content
+    const mainContent = document.getElementById("main-content");
+    mainContent.style.display = "block";
+  });
+  
